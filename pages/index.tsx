b445c7e245ac3f2c5c2ad5b77759e9589536feea
FabioRocha231 <fabioharoldo1@gmail.com>
2022-04-28 10:44:29 -0300

    feat: improving metaTags

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
+import type { NextPage } from "next";
+import Head from "next/head";
 
 import {
   AboutMe,
@@ -8,29 +8,38 @@ import {
   MyRepos,
   MySkill,
   NavBar,
-  SocialLinks
-} from '../components'
+  SocialLinks,
+} from "../components";
 
 const Home: NextPage = () => {
   return (
     <section className="bg-[#262626]">
       <Head>
-        <title>Fabio HR Filho</title>
+        <title>Fabio HR Filho - FullStack JS</title>
         <meta name="description" content="Fabio Hr - Fullstack Developer" />
         <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1"
+          key="viewport"
+        />
+        <meta
+          name="keywords"
+          content="FullStack, Javascript, Expo, Typescrip, NextJs, NodeJs, BackEnd, HTML5, CSS3, Git, Express, MongoDB, PostgresSql, FrontEnd, Blockchain, Wax, Solana"
+        />
       </Head>
 
       <NavBar />
       <main className="flex w-full flex-col items-center justify-center">
         <Header />
-        <AboutMe id={'about'} />
-        <MySkill id={'skills'} />
+        <AboutMe id={"about"} />
+        <MySkill id={"skills"} />
         <BlockchainExperience />
         <MyRepos />
         <SocialLinks />
       </main>
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
