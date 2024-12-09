f4c37ecf692fee178f24a2a51cad2892744b8a2c
FabioRocha231 <fabioharoldo1@gmail.com>
2024-12-09 09:04:01 -0300

    refactor: improved Redirect component

--- components/Greetings/Redirect.tsx
+++ components/Greetings/Redirect.tsx
@@ -8,11 +8,9 @@ export interface RedirectProps {
 
 const Redirect = ({ to, content }: RedirectProps) => {
   return (
-    <Link href={to}>
-      <span className="rounded-xl bg-slate-300 p-2 text-lg font-medium underline lg:text-slate-500 xl:p-1 xs:text-base">
-        {content}
-      </span>
-    </Link>
+    <span className="rounded-xl bg-slate-300 p-2 text-lg font-medium underline lg:text-slate-500 xl:p-1 xs:text-base">
+      <Link href={to}>{content}</Link>
+    </span>
   );
 };
 
