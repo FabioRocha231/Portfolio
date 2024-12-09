8b5ec90dafd9eb5c5ad392fb43d0ca9c0cd01288
FabioRocha231 <fabioharoldo1@gmail.com>
2024-12-09 09:04:21 -0300

    fix: fixed h1 text color

--- components/Greetings/TextAndRedirect.tsx
+++ components/Greetings/TextAndRedirect.tsx
@@ -12,7 +12,7 @@ export const TextAndRedirect = () => {
   return (
     <div className="flex h-full flex-col items-center justify-center">
       <section className="flex self-start w-[650px] flex-col 2xl:w-[500px] lg:w-full">
-        <h1 className="text-5xl font-black lg:text-5xl lg:text-white sm:text-3xl xs:text-2xl ">
+        <h1 className="text-5xl text-black lg:text-5xl lg:text-white sm:text-3xl xs:text-2xl ">
           Ola, me chamo Fabio Rocha
         </h1>
         <ReactTyped
@@ -29,13 +29,11 @@ export const TextAndRedirect = () => {
           className="text-xl font-medium
           text-slate-500 lg:text-white xs:text-lg"
         >
-          Atuo como Desenvolvedor Fullstack há 5 anos, com experiência em React,
-          React Native, Next.js, Node.js TypeScript e Golang. Ficou
+          Atuo como Desenvolvedor Fullstack há 5+ anos, com experiência em
+          React, React Native, Next.js, Node.js TypeScript e Golang. Ficou
           interessado(a) e deseja saber mais {""}
           <Redirect to="/about" content="Sobre mim" />
-          {""}? Confira minhas {""}
-          <Redirect to="/skills" content="habilidades" />, {""} ou faça download
-          meu {""}
+          {""}? {""} faça download meu {""}
           <Redirect to="/curriculum" content="curriculum" />
           {""} ou entre em {""}
           <Redirect to="/contact" content="contato" />
