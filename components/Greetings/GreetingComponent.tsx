0de8d83d8a3cbdde4ad02c330f9d5224113a23ff
FabioRocha231 <fabioharoldo1@gmail.com>
2024-12-09 09:04:47 -0300

    feat: placed MySkills in GrettingsComponent

--- components/Greetings/GreetingComponent.tsx
+++ components/Greetings/GreetingComponent.tsx
@@ -1,9 +1,11 @@
+import { MySkills } from '../MySkills/MySkills'
 import { TextAndRedirect } from './TextAndRedirect'
 
 export const GreetingsComponent = () => {
   return (
     <div className="flex h-full flex-1 flex-col bg-slate-100 px-20 lg:hidden">
       <TextAndRedirect />
+      <MySkills />
     </div>
   )
 }
